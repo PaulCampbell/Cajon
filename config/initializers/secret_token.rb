46e67538567a2db85f40dc5667c8c01693703f0e
paulcampbell <paulgcampbell@gmail.com>
2010-10-09 10:38:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cajon::Application.config.secret_token = '7b6a0d8c52d4addebeb39fbc1d97b6ebbf076702b579c90bed3263970a6a9449e0e5992ea1b2e99cca979d0e0f3e784ad9f443885d5db83413c176cd008560a7'
