19c24d0451d862db1b96e9e70d4691609e7d6b31
paulcampbell <paulgcampbell@gmail.com>
2010-10-10 13:03:52 +0100

    Authentication code... getting there.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Cajon::Application.routes.draw do
+  get "sessions/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
 
   resources :posts
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
+  
+  
+  
+  
   root :to => 'pages#home'
   
   
