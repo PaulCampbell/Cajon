0c1ed24bc1f98f64e20fe01f1299ba651879f982
paulcampbell <paulgcampbell@gmail.com>
2010-10-15 00:07:44 +0100

    refactored some views to remove duplication...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,14 @@ module ApplicationHelper
 				"#{base_title} | #{@title}"
 		end
 	end
+	
+	def format_date(date, use_time = false)
+    if use_time == true
+        ampm = date.strftime("%p").downcase
+        new_date = date.strftime("%B %d, %Y at %I:%M" + ampm)
+    else
+        new_date = date.strftime("%B %d, %Y")
+    end
+end
 
 end
