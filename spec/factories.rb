4b89a242f7492e3fbda7bb5bb240b1ebd31cc190
paulcampbell <paulgcampbell@gmail.com>
2011-09-12 09:30:00 +0100

    Text updates

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
   user.name                  "Paul Campbell"
-  user.email                 "paulcampbell@example.com"
+  user.email                 "paulcampbell@example.com" 
   user.password              "someword"
   user.password_confirmation "someword"
 end
